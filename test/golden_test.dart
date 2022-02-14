83d29738b3228fe7cab7c9ef79988ebb02ba0b8e
yukiyamamuro <yuki.sumaho@gmail.com>
2022-02-14 11:23:16 +0900

    golden test setting

--- test/golden_test.dart
+++ test/golden_test.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_test/flutter_test.dart';
 import 'package:golden_toolkit/golden_toolkit.dart';
 import 'package:riverpod_app/main.dart';
 import 'package:riverpod_app/view_model.dart';
 
 void main() {
+  setUpAll(() async {
+    await loadAppFonts();
+  });
   testGoldens('normal', (tester) async {
     const iPhoneN = Device(
       name: 'iPhoneN',
