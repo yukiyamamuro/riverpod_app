15033aa39daa6d48c98b9f0bca2b092ac45aa2ce
yukiyamamuro <yuki.sumaho@gmail.com>
2022-02-15 09:12:42 +0900

    add mock test

--- test/golden_test.dart
+++ test/golden_test.dart
@@ -3,22 +3,26 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:golden_toolkit/golden_toolkit.dart';
+import 'package:mocktail/mocktail.dart';
 import 'package:riverpod_app/main.dart';
 import 'package:riverpod_app/view_model.dart';
+
+class MockViewModel extends Mock implements ViewModel {}
 
 void main() {
   setUpAll(() async {
     await loadAppFonts();
   });
+
+  const iPhoneN = Device(
+    name: 'iPhoneN',
+    size: Size(414, 736),
+    devicePixelRatio: 3.0,
+  );
+
+  List<Device> devices = [iPhoneN];
+
   testGoldens('normal', (tester) async {
-    const iPhoneN = Device(
-      name: 'iPhoneN',
-      size: Size(414, 736),
-      devicePixelRatio: 3.0,
-    );
-
-    List<Device> devices = [iPhoneN];
-
     ViewModel viewModel = ViewModel();
 
     await tester.pumpWidgetBuilder(ProviderScope(
@@ -41,5 +45,44 @@ void main() {
       'myHomePage_1tapped',
       devices: devices,
     );
+  });
+
+  testGoldens('viewModelTest', (tester) async {
+    var mock = MockViewModel();
+    when(() => mock.count).thenReturn(123456789.toString());
+    when(() => mock.countUp).thenReturn(223456789.toString());
+    when(() => mock.countDown).thenReturn(323456789.toString());
+
+    await tester.pumpWidgetBuilder(
+      ProviderScope(
+        child: MyHomePage(mock),
+      ),
+    );
+
+    await multiScreenGolden(
+      tester,
+      'myHomePage_mock',
+      devices: devices,
+    );
+
+    verifyNever(() => mock.onIncrease());
+    verifyNever(() => mock.onDecrease());
+    verifyNever(() => mock.onReset());
+
+    await tester.tap(find.byIcon(CupertinoIcons.plus));
+    verify(() => mock.onIncrease()).called(1);
+    verifyNever(() => mock.onDecrease());
+    verifyNever(() => mock.onReset());
+
+    await tester.tap(find.byIcon(CupertinoIcons.minus));
+    await tester.tap(find.byIcon(CupertinoIcons.minus));
+    verifyNever(() => mock.onIncrease());
+    verify(() => mock.onDecrease()).called(2);
+    verifyNever(() => mock.onReset());
+
+    await tester.tap(find.byIcon(Icons.refresh));
+    verifyNever(() => mock.onIncrease());
+    verifyNever(() => mock.onDecrease());
+    verify(() => mock.onReset()).called(1);
   });
 }
